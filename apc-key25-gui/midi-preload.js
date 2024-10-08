232d97f3042208507c0d56e8edc86bd6224abeab
TheDeadScythe <knechtmaxime@gmail.com>
2024-10-08 18:12:42 +0200

    👌 IMPROVE

--- apc-key25-gui/midi-preload.js
+++
@@ -1,8 +0,0 @@
-// midi-preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose ipcRenderer to the web page
-contextBridge.exposeInMainWorld('electronAPI', {
-    receiveMidiPorts: (callback) => ipcRenderer.on('midi-ports', (event, ports) => callback(event, ports)),
-    connectMidi: (ports) => ipcRenderer.send('connect-midi', ports),
-});
